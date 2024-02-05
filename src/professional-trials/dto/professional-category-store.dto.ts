1bd507d31d200c006b16c88d0b5013e939ce3412
Niko <shilenkok@bk.ru>
2024-02-05 18:08:57 +0300

    last

--- src/professional-trials/dto/professional-category-store.dto.ts
+++ src/professional-trials/dto/professional-category-store.dto.ts
@@ -1,6 +1,11 @@
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class StoreProfessionalCategoryDto {
+  @ApiProperty({
+    example: 'Some title',
+    description: 'Title of professional trial category',
+  })
   @MaxLength(256)
   @IsString()
   @IsNotEmpty()
