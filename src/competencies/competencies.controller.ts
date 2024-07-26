728cd451243027ea369062682b840ac1ead261f3
KrekcrRoot <shilenkok@bk.ru>
2024-07-26 21:00:47 +0300

    [~] Competence api docs update

--- src/competencies/competencies.controller.ts
+++ src/competencies/competencies.controller.ts
@@ -1,45 +1,86 @@
-import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CompetenciesService } from './competencies.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CompetenceStoreDto } from './dto/competenceStore.dto';
 import { AccessTokenGuard } from '../common/guards/accessToken.guard';
 import { Roles } from '../roles/roles.decorator';
 import { UserRole } from '../roles/role.enum';
 import { RolesGuard } from '../roles/roles.guard';
+import { Competence } from './competence.entity';
+import { DeleteResult } from 'typeorm';
 
 @ApiTags('Competencies')
 @Controller('competencies')
 export class CompetenciesController {
-
   constructor(private competenciesService: CompetenciesService) {}
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: Competence,
+    description: 'Get all competencies',
+  })
+  @ApiOperation({ summary: 'Get all competencies' })
   @Get('/all')
   getAll() {
     return this.competenciesService.findAll();
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: Competence,
+    description: 'Get competence by title',
+  })
+  @ApiOperation({ summary: 'Get competence by title' })
   @Get('/byTitle/:title')
-  getByTitle(@Param() params: {title: string}) {
+  getByTitle(@Param() params: { title: string }) {
     return this.competenciesService.findByTitle(params.title);
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: Competence,
+    description: 'Get competence by uuid',
+  })
+  @ApiOperation({ summary: 'Get competence by uuid' })
   @Get('/:uuid')
-  getByUuid(@Param() params: {uuid: string}) {
+  getByUuid(@Param() params: { uuid: string }) {
     return this.competenciesService.findByUuid(params.uuid);
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: DeleteResult,
+    description: 'Delete competence',
+  })
+  @ApiOperation({ summary: 'Delete competence' })
+  @ApiBearerAuth()
   @UseGuards(AccessTokenGuard, RolesGuard)
   @Roles(UserRole.Moderator)
   @Delete('/:uuid')
-  deleteByUuid(@Param() params: {uuid: string}) {
+  deleteByUuid(@Param() params: { uuid: string }) {
     return this.competenciesService.deleteByUuid(params.uuid);
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: Competence,
+    description: 'Create competence',
+  })
+  @ApiOperation({ summary: 'Create competence' })
+  @ApiBearerAuth()
   @UseGuards(AccessTokenGuard, RolesGuard)
   @Roles(UserRole.Moderator)
   @Post('/store')
   storeCompetence(@Body() competenceStoreDto: CompetenceStoreDto) {
     return this.competenciesService.store(competenceStoreDto);
   }
-
 }
--- src/competencies/dto/competenceStore.dto.ts
+++ src/competencies/dto/competenceStore.dto.ts
@@ -1,7 +1,12 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CompetenceStoreDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Компетенция',
+    description: 'Title of competence',
+  })
   title: string;
 }
